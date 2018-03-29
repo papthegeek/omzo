4f7cfbb0e0666cdfd81d455399ba00d1c61aee23
papthegeek <pgueye01@gmail.com>
2018-03-28 23:11:17 -0400

    Paypal checkout commit

---
+++ app/src/main/java/com/test/papthegeek/demoproject/Config.java
@@ -0,0 +1,9 @@
+package com.test.papthegeek.demoproject;
+
+/**
+ * Created by papthegeek on 3/17/18.
+ */
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID =  "AbmlU9gACOOQQK0I4GZF6IolGfvenH_tqC4pArgrY0Zl3DeYb9_rT3nY3zTWuGhAQOyRTFg03X-LIJfP"; // TO BE DETERMINED
+}
--- app/src/main/java/com/test/papthegeek/demoproject/detailActivity.java
+++ app/src/main/java/com/test/papthegeek/demoproject/detailActivity.java
@@ -1,13 +1,29 @@
 package com.test.papthegeek.demoproject;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.braintreepayments.api.PayPal;
 import com.bumptech.glide.Glide;
+import com.paypal.android.sdk.payments.PayPalConfiguration;
+import com.paypal.android.sdk.payments.PayPalPayment;
+import com.paypal.android.sdk.payments.PayPalPaymentDetails;
+import com.paypal.android.sdk.payments.PayPalService;
+import com.paypal.android.sdk.payments.PaymentActivity;
+import com.paypal.android.sdk.payments.PaymentConfirmation;
+
+import org.json.JSONException;
+
+import java.math.BigDecimal;
 
 public class detailActivity extends AppCompatActivity {
 
@@ -18,6 +34,23 @@ public class detailActivity extends AppCompatActivity {
     TextView description;
     ImageView image;
     Context context;
+    Button btnPayNow;
+    EditText edtAmount;
+
+    String amount = "";
+
+    public static final int PAYPAL_REQUEST_CODE = 1094;
+
+    private static PayPalConfiguration config = new PayPalConfiguration()
+            .environment(PayPalConfiguration.ENVIRONMENT_SANDBOX)  // Use Sandbox for testing;
+            .clientId(Config.PAYPAL_CLIENT_ID);
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopService(new Intent(this, PayPalService.class));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +58,19 @@ public class detailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         System.out.println("DetailActivity has been triggered!!!!");
 
+        //START PAYPAL SERVICE
+        Intent paypalIntent = new Intent(this, PayPalService.class);
+        paypalIntent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION, config);
+        startService(paypalIntent);
+
+
         //INITIALIZE VIEW
         title = (TextView) findViewById(R.id.detailTitle);
         image = (ImageView) findViewById(R.id.detailImage);
         price = (TextView) findViewById(R.id.detailprice);
         date = (TextView) findViewById(R.id.detailDate);
+        btnPayNow = (Button) findViewById(R.id.btnPayNow);
+        edtAmount = (EditText) findViewById(R.id.edtAmount);
 
         //RECEIVE DATA
         Intent intent = this.getIntent();
@@ -45,5 +86,48 @@ public class detailActivity extends AppCompatActivity {
         date.setText(eventDate);
         Glide.with(this).load(img).into(image);
 
+
+        btnPayNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                processPayment();
+            }
+        });
+
+    }
+
+    private void processPayment() {
+        amount = edtAmount.getText().toString();
+        PayPalPayment payPalPayment = new PayPalPayment(new BigDecimal(String.valueOf(amount)),    "USD", "Purchase on SenPro" , PayPalPayment.PAYMENT_INTENT_SALE);
+
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(PayPalService.EXTRA_PAYPAL_CONFIGURATION,config) ;
+        intent.putExtra(PaymentActivity.EXTRA_PAYMENT, payPalPayment);
+        startActivityForResult(intent, PAYPAL_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == PAYPAL_REQUEST_CODE){
+            if(resultCode == RESULT_OK){
+                PaymentConfirmation confirmation = data.getParcelableExtra(PaymentActivity.EXTRA_RESULT_CONFIRMATION);
+                if(confirmation != null){
+                    try {
+                        String paymentDetails = confirmation.toJSONObject().toString(4 );
+                        startActivity(new Intent(this, PaymentDetails.class)
+                                .putExtra("PaymentDetails", paymentDetails)
+                                .putExtra("PaymentAmount", amount)
+                        );
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+            else if (resultCode == Activity.RESULT_CANCELED)
+                Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show();
+        }
+        else if (resultCode == PaymentActivity.RESULT_EXTRAS_INVALID)
+            Toast.makeText(this, "Invalid", Toast.LENGTH_SHORT);
     }
 }
